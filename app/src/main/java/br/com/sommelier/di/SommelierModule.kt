cc44b138b9acdf0cb156f52a1f9c12525e2ff37f
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-15 20:42:31 -0400

    [add-user-repository] Setup SommelierModule with dataModule

--- app/src/main/java/br/com/sommelier/di/SommelierModule.kt
+++ app/src/main/java/br/com/sommelier/di/SommelierModule.kt
@@ -1,3 +1,26 @@
 package br.com.sommelier.di
 
-private fun provideFirestore(): Nothing = TODO()
+import br.com.sommelier.util.FirestoreCollections
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import org.koin.dsl.module
+
+object SommelierModule {
+
+    private val dataModule = module {
+        factory { provideFirestore() }
+        factory { provideUsersCollection(get()) }
+    }
+
+    private fun provideUsersCollection(firestore: FirebaseFirestore): CollectionReference {
+        return firestore.collection(FirestoreCollections.USERS)
+    }
+
+    private fun provideFirestore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    fun getModules() = listOf(dataModule)
+}
---
+++ app/src/main/java/br/com/sommelier/util/FirestoreCollections.kt
@@ -0,0 +1,5 @@
+package br.com.sommelier.util
+
+object FirestoreCollections {
+    const val USERS = "users"
+}
