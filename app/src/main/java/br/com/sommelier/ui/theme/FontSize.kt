d9889cca62de0af8888df77843755141f6c7ebba
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 10:24:09 -0400

    [setup-typography] Create FontSize object

---
+++ app/src/main/java/br/com/sommelier/ui/theme/FontSize.kt
@@ -0,0 +1,15 @@
+package br.com.sommelier.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.sp
+
+@Immutable
+object FontSize {
+    val smallest = 10.sp
+    val extraSmall = 12.sp
+    val small = 14.sp
+    val normal = 16.sp
+    val medium = 18.sp
+    val large = 20.sp
+    val largest = 24.sp
+}
