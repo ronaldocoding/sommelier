6c8e525bb9aeacbcc46c907b172cb0338e7faf9c
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 19:21:56 -0400

    [setup-sizing-spacing] Create SpacingInset object

---
+++ app/src/main/java/br/com/sommelier/ui/theme/SpacingInset.kt
@@ -0,0 +1,24 @@
+package br.com.sommelier.ui.theme
+
+import androidx.compose.runtime.Immutable
+
+@Suppress("unused")
+@Immutable
+object SpacingInset {
+    val none = Spacing.none
+    val smallest = Spacing.smallest
+    val extraSmaller = Spacing.extraSmaller
+    val smaller = Spacing.smaller
+    val extraSmall = Spacing.extraSmall
+    val small = Spacing.small
+    val normal = Spacing.normal
+    val medium = Spacing.medium
+    val mediumLarge = Spacing.mediumLarge
+    val large = Spacing.large
+    val extraLarge = Spacing.extraLarge
+    val larger = Spacing.larger
+    val extraLarger = Spacing.extraLarger
+    val largest = Spacing.largest
+    val huge = Spacing.huge
+    val extraHuge = Spacing.extraHuge
+}
