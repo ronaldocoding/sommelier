b0325491c0d5e32b6078cc41c8f310e85c3ada8f
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 10:38:30 -0400

    [setup-typography] Remove unused LineHeight property

--- app/src/main/java/br/com/sommelier/ui/theme/LineHeight.kt
+++ app/src/main/java/br/com/sommelier/ui/theme/LineHeight.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.unit.sp
 @Immutable
 object LineHeight {
     val smallest = 16.sp
-    val small = 18.sp
     val normal = 20.sp
     val medium = 24.sp
     val large = 28.sp
