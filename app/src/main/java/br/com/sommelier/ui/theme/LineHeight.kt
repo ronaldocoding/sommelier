d8702ea1810f8329838c5fcff49f349a7cecf435
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 10:24:55 -0400

    [setup-typography] Create LineHeight object

---
+++ app/src/main/java/br/com/sommelier/ui/theme/LineHeight.kt
@@ -0,0 +1,14 @@
+package br.com.sommelier.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.sp
+
+@Immutable
+object LineHeight {
+    val smallest = 16.sp
+    val small = 18.sp
+    val normal = 20.sp
+    val medium = 24.sp
+    val large = 28.sp
+    val largest = 32.sp
+}
