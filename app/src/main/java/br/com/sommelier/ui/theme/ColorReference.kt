53dd5bb5f5695a29137cf5c1a106e3e6f6632fb2
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 23:16:24 -0400

    [setup-colors-and-border-radius] Create ColorReference object

---
+++ app/src/main/java/br/com/sommelier/ui/theme/ColorReference.kt
@@ -0,0 +1,27 @@
+package br.com.sommelier.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+
+@Suppress("unused")
+@Immutable
+object ColorReference {
+    val white = Color(0xffffffff)
+    val taupeGray = Color(0xff8b8585)
+    val quartz = Color(0xff49454f)
+    val royalPurple = Color(0xff6750A4)
+    val gainsBoro = Color(0xffdcdcdc)
+    val sunGlow = Color(0xffffd43a)
+    val sonicSilver = Color(0xff79747e)
+    val snow = Color(0xfffffbfe)
+    val orangeRed = Color(0xffff4e4e)
+    val blueberry = Color(0xff438eff)
+    val forestGreen = Color(0xff50a773)
+    val chineseSilver = Color(0xffcacaca)
+    val frenchFuchsia = Color(0xffff4893)
+    val antiFlashWhite = Color(0xfff0f0f0)
+    val brightGray  = Color(0xffeeeeee)
+    val eerieBlack = Color(0xff1f1f1f)
+    val black = Color(0xff000000)
+    val lightSilver = Color(0xffd6d6d6)
+}
