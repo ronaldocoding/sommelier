cf9ea2fc829f1b783c36e900e1e9480674450b5b
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-26 22:15:10 -0400

    [setup-colors-and-border-radius] Create BorderRadius object

---
+++ app/src/main/java/br/com/sommelier/ui/theme/BorderRadius.kt
@@ -0,0 +1,14 @@
+package br.com.sommelier.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.unit.dp
+
+@Suppress("unused")
+@Immutable
+object BorderRadius {
+    val none = Sizing.none
+    val small = 4.dp
+    val medium = 8.dp
+    val large = 16.dp
+    val extraLarge = 24.dp
+}
