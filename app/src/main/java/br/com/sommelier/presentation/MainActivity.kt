0bce4bf08fedfe4537e51dd2280c5aa8db5d799d
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-30 14:04:31 -0400

    [add-quick-action-button] Add QuickActionButton to MainActivity

--- app/src/main/java/br/com/sommelier/presentation/MainActivity.kt
+++ app/src/main/java/br/com/sommelier/presentation/MainActivity.kt
@@ -3,14 +3,19 @@ package br.com.sommelier.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.sommelier.ui.component.ActionButton
+import br.com.sommelier.ui.component.QuickActionButton
 import br.com.sommelier.ui.theme.SommelierTheme
 import br.com.sommelier.ui.theme.Spacing
 
@@ -23,11 +28,17 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ActionButton(
-                        text = "Login",
-                        onClick = {},
-                        modifier = Modifier.padding(horizontal = Spacing.small)
-                    )
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        ActionButton(
+                            text = "Login",
+                            modifier = Modifier.padding(horizontal = Spacing.small)
+                        )
+                        Spacer(modifier = Modifier.padding(Spacing.small))
+                        QuickActionButton()
+                    }
                 }
             }
         }
@@ -42,11 +53,17 @@ fun SommelierPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            ActionButton(
-                text = "Login",
-                onClick = { /*TODO*/ },
-                modifier = Modifier.padding(horizontal = Spacing.mediumLarge)
-            )
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                ActionButton(
+                    text = "Login",
+                    modifier = Modifier.padding(horizontal = Spacing.small)
+                )
+                Spacer(modifier = Modifier.padding(Spacing.small))
+                QuickActionButton()
+            }
         }
     }
 }
