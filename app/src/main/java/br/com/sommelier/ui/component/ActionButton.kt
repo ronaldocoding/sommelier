d98ffcbb55cfe8578e556eacde7a876726722d02
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-07-29 23:20:28 -0400

    [add-action-button] Improve ActionButton implementation

--- app/src/main/java/br/com/sommelier/ui/component/ActionButton.kt
+++ app/src/main/java/br/com/sommelier/ui/component/ActionButton.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import br.com.sommelier.ui.theme.ColorReference
@@ -16,34 +17,50 @@ import br.com.sommelier.ui.theme.Typography
 
 @Composable
 fun ActionButton(
-    text: String,
-    onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    text: String = "Action",
+    onClick: () -> Unit = {},
     textColor: Color = ColorReference.white,
     textStyle: TextStyle = Typography.bodyMedium,
     backGroundColor: Color = ColorReference.royalPurple,
     isOutlined: Boolean = false
 ) {
-    Box(modifier = modifier) {
+    Box(
+        modifier = modifier.testTag("ActionButton")
+    ) {
         if (isOutlined) {
             OutlinedButton(
                 onClick = onClick,
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .testTag("OutlinedActionButton"),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = backGroundColor
                 )
             ) {
-                Text(text = text, style = textStyle, color = textColor)
+                Text(
+                    text = text,
+                    style = textStyle,
+                    color = textColor,
+                    modifier = Modifier.testTag("OutlinedActionButtonText")
+                )
             }
         } else {
             Button(
                 onClick = onClick,
-                modifier = modifier.fillMaxWidth(),
+                modifier = modifier
+                    .fillMaxWidth()
+                    .testTag("NormalActionButton"),
                 colors = ButtonDefaults.buttonColors(
                     containerColor = backGroundColor
                 )
             ) {
-                Text(text = text, style = textStyle, color = textColor)
+                Text(
+                    text = text,
+                    style = textStyle,
+                    color = textColor,
+                    modifier = Modifier.testTag("NormalActionButtonText")
+                )
             }
         }
     }
@@ -52,18 +69,13 @@ fun ActionButton(
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ActionButtonPreviewPrimary() {
-    ActionButton(
-        text = "Action",
-        onClick = {}
-    )
+    ActionButton()
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ActionButtonPreviewSecondary() {
     ActionButton(
-        text = "Action",
-        onClick = {},
         backGroundColor = ColorReference.frenchFuchsia
     )
 }
@@ -72,8 +84,6 @@ fun ActionButtonPreviewSecondary() {
 @Composable
 fun ActionButtonPreviewTertiary() {
     ActionButton(
-        text = "Action",
-        onClick = {},
         backGroundColor = ColorReference.white,
         textColor = ColorReference.royalPurple,
         isOutlined = true
