a13c92babd7d6c5dce0f8649e790ba7aba6d3b3c
ronaldocosta <ronaldocosta.developer@gmail.com>
2023-08-01 20:01:34 -0400

    [add-outlined-text-input] Create OutlinedTextInput component

--- app/src/main/java/br/com/sommelier/presentation/MainActivity.kt
+++ app/src/main/java/br/com/sommelier/presentation/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -22,12 +28,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import br.com.sommelier.R
 import br.com.sommelier.ui.component.ActionButton
 import br.com.sommelier.ui.component.ClickableText
+import br.com.sommelier.ui.component.OutlinedTextInput
 import br.com.sommelier.ui.component.QuickActionButton
 import br.com.sommelier.ui.component.SommelierTopBar
 import br.com.sommelier.ui.component.SommelierTopBarButton
 import br.com.sommelier.ui.theme.ColorReference
 import br.com.sommelier.ui.theme.SommelierTheme
 import br.com.sommelier.ui.theme.Spacing
+import br.com.sommelier.ui.theme.Typography
+import br.com.sommelier.util.emptyString
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,11 +53,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Content() {
     SommelierTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = ColorReference.white
-        ) {
-            Box {
+        Scaffold(
+            topBar = {
                 SommelierTopBar(
                     leftButton = SommelierTopBarButton.Enabled(
                         icon = Icons.Default.ArrowBack,
@@ -59,17 +65,56 @@ fun Content() {
                         contentDescription = "Edit"
                     )
                 )
-            }
+            },
+            floatingActionButton = {
+                QuickActionButton()
+            },
+            containerColor = ColorReference.white
+        ) {
             Column(
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxHeight(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                ActionButton(
-                    text = "Login",
-                    modifier = Modifier.padding(horizontal = Spacing.small)
+
+                var text by rememberSaveable { mutableStateOf(emptyString()) }
+                var textInputErrorMessage by rememberSaveable { mutableStateOf(emptyString()) }
+                var textInputIsError by rememberSaveable { mutableStateOf(false) }
+                var textInputIsEnabled by rememberSaveable { mutableStateOf(true) }
+
+                OutlinedTextInput(
+                    modifier = Modifier.padding(horizontal = Spacing.mediumLarge),
+                    value = text,
+                    onValueChange = { changedValue ->
+                        text = changedValue
+                    },
+                    placeholder = "Type your name",
+                    label = "Name",
+                    leadingIcon = ImageVector.vectorResource(id = R.drawable.ic_user),
+                    isError = textInputIsError,
+                    supportingText = {
+                        Text(text = textInputErrorMessage, style = Typography.label)
+                    },
+                    isEnabled = textInputIsEnabled
                 )
                 Spacer(modifier = Modifier.padding(Spacing.small))
-                QuickActionButton()
+                ActionButton(
+                    text = "Login",
+                    modifier = Modifier.padding(horizontal = Spacing.small),
+                    onClick = {
+                        if (text.isEmpty()) {
+                            textInputErrorMessage = "Please, type your name"
+                            textInputIsError = true
+                            textInputIsEnabled = true
+                        } else {
+                            textInputErrorMessage = emptyString()
+                            textInputIsError = false
+                            textInputIsEnabled = false
+                        }
+                    }
+                )
                 Spacer(modifier = Modifier.padding(Spacing.small))
                 ClickableText(
                     nonClickableText = "Don't have an account? ",
---
+++ app/src/main/java/br/com/sommelier/ui/component/OutlinedTextInput.kt
@@ -0,0 +1,145 @@
+package br.com.sommelier.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.sommelier.R
+import br.com.sommelier.ui.theme.ColorReference
+import br.com.sommelier.ui.theme.Typography
+import br.com.sommelier.util.emptyString
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OutlinedTextInput(
+    modifier: Modifier = Modifier,
+    value: String = emptyString(),
+    valueStyle: TextStyle = Typography.bodyLarge,
+    onValueChange: (String) -> Unit = {},
+    placeholder: String = emptyString(),
+    placeholderStyle: TextStyle = Typography.bodyLarge,
+    placeHolderColor: Color = ColorReference.taupeGray,
+    label: String = emptyString(),
+    focusedLabelStyle: TextStyle = Typography.bodyLarge,
+    unfocusedLabelStyle: TextStyle = Typography.label,
+    labelColor: Color = ColorReference.taupeGray,
+    leadingIcon: ImageVector? = null,
+    leadingIconContentDescription: String? = null,
+    isError: Boolean = false,
+    isEnabled: Boolean = true,
+    supportingText: (@Composable () -> Unit)? = null,
+) {
+
+    var isLabelFocused by remember { mutableStateOf(false) }
+    var isTextFieldFocused by remember { mutableStateOf(false) }
+
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier
+            .fillMaxWidth()
+            .onFocusChanged {
+                isTextFieldFocused = it.isFocused
+            }
+            .testTag("OutlinedTextField"),
+        textStyle = valueStyle,
+        enabled = isEnabled,
+        isError = isError,
+        supportingText = supportingText,
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = ColorReference.royalPurple,
+            unfocusedBorderColor = ColorReference.taupeGray,
+        ),
+        label = {
+            Text(
+                text = label,
+                style = if (isTextFieldFocused) {
+                    unfocusedLabelStyle
+                } else if (!isLabelFocused && value.isEmpty()) {
+                    focusedLabelStyle
+                } else {
+                    unfocusedLabelStyle
+                },
+                color = labelColor,
+                modifier = Modifier
+                    .onFocusChanged {
+                        isLabelFocused = it.isFocused
+                    }
+                    .testTag("OutlinedTextFieldLabel")
+            )
+        },
+        placeholder = {
+            Text(
+                text = placeholder,
+                style = placeholderStyle,
+                color = placeHolderColor,
+                modifier = Modifier.testTag("OutlinedTextFieldPlaceholder")
+            )
+        },
+        leadingIcon = if (leadingIcon != null) {
+            {
+                Icon(
+                    imageVector = leadingIcon,
+                    contentDescription = leadingIconContentDescription,
+                    tint = ColorReference.taupeGray,
+                    modifier = Modifier.testTag("OutlinedTextFieldLeadingIcon")
+                )
+            }
+        } else {
+            null
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OutlinedTextInputDefaultPreview() {
+    var text by rememberSaveable { mutableStateOf(emptyString()) }
+    OutlinedTextInput(value = text, onValueChange = { text = it })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OutlinedTextInputPreview() {
+    var text by rememberSaveable { mutableStateOf(emptyString()) }
+    OutlinedTextInput(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = "Type your name",
+        label = "Name",
+        leadingIcon = ImageVector.vectorResource(id = R.drawable.ic_user)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OutlinedTextInputErrorPreview() {
+    var text by rememberSaveable { mutableStateOf(emptyString()) }
+    OutlinedTextInput(
+        value = text,
+        onValueChange = { text = it },
+        placeholder = "Type your name",
+        label = "Name",
+        leadingIcon = ImageVector.vectorResource(id = R.drawable.ic_user),
+        isError = true,
+        supportingText = {
+            Text(text = "Error message", style = Typography.label)
+        }
+    )
+}
